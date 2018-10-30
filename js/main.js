b7716faf44c7711c5dd46726a5c0279f749b6abc
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-10-30 13:59:01 +0100

    Adding boilerplate

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Exercice n°1
+
+
+var user = {
+  firstName: 'Andrew',
+  lastName: 'Toto',
+  country: 'France',
+  years: '22',
+  // card = [
+  //   {
+  //     name: 'visa'
+  //   },
+  //   {
+  //     name: 'mastercard'
+  //   },
+  // ],
+}
+
+var firstName = document.getElementById('firstName').value = user.firstName;
+var lastName = document.getElementById('lastName').value = user.lastName;
+var country = document.getElementById('country').value = user.country;
+var years = document.getElementById('years').value = user.years;
+// var cards = document.getElementById('card').value = user.card.;
+
